944d06527c16c4b9c6824a5a2b86ea6ca04d0fdb
pkj7323 <pkj7323@naver.com>
2025-01-25 16:08:43 +0900

    템플릿 메소드를 일반화 해보자

--- DesignPattern/main.cpp
+++ DesignPattern/main.cpp
@@ -1,60 +1,26 @@
 #include <iostream>
-class caffeineBeverage
-{
-public:
-	virtual void prepareRecipe() = 0;
-	void boilWater()
-	{
-		std::cout << "Boiling water" << std::endl;
-	}
-	void pourInCup()
-	{
-		std::cout << "Pouring into cup" << std::endl;
-	}
-};
-class Tea : public caffeineBeverage
-{
-public:
-	void prepareRecipe()
-	{
-		boilWater();
-		steepTeaBag();
-		pourInCup();
-		addLemon();
-	}
-	void steepTeaBag()
-	{
-		std::cout << "Steeping the tea" << std::endl;
-	}
-	void addLemon()
-	{
-		std::cout << "Adding Lemon" << std::endl;
-	}
-};
-class Coffee : public caffeineBeverage
-{
-public:
-	void prepareRecipe()
-	{
-		boilWater();
-		brewCoffeeGrinds();
-		pourInCup();
-		addSugarAndMilk();
-	}
-	void brewCoffeeGrinds()
-	{
-		std::cout << "Dripping Coffee through filter" << std::endl;
-	}
-	void addSugarAndMilk()
-	{
-		std::cout << "Adding Sugar and Milk" << std::endl;
-	}
-};
+class AbstractClass
+{//메소드 = 멤버함수
+	virtual void templetMethod() final // 템플릿 메소드
+	{	// 클래스의 메소드이면서
+		//일종의 알고리즘을 정의하도록 유도하는 템플릿인것
 
+		primativeOperation1();// 추상 메소드 1
+		primativeOperation2();// 추상 메소드 2
+		concreteOperation(); // 구상 메소드
+	}
+	virtual void primativeOperation1() = 0;// 추상 메소드 1
+	virtual void primativeOperation2() = 0;// 추상 메소드 2
+	void concreteOperation()// 구상 메소드
+	{
+		std::cout << "구상 함수입니다." << std::endl;
+	}
+};
 
 
 
 int main() {
 	
+
 	return 0;
 }
