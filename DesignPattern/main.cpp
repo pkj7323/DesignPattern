13199f46ffe0924cd24126a0b8cdb11388c379a4
pkj7323 <pkj7323@naver.com>
2025-01-25 16:11:16 +0900

    후크 를 사용하는예시

--- DesignPattern/main.cpp
+++ DesignPattern/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-class AbstractClass
-{//메소드 = 멤버함수
-	virtual void templetMethod() final // 템플릿 메소드
-	{	// 클래스의 메소드이면서
-		//일종의 알고리즘을 정의하도록 유도하는 템플릿인것
-
-		primativeOperation1();// 추상 메소드 1
-		primativeOperation2();// 추상 메소드 2
-		concreteOperation(); // 구상 메소드
+class CaffeineBeverageWithHook {
+public:
+	void prepareRecipe() {
+		boilWater();
+		brew();
+		pourInCup();
+		if (customerWantsCondiments()) {
+			addCondiments();
+		}
 	}
-	virtual void primativeOperation1() = 0;// 추상 메소드 1
-	virtual void primativeOperation2() = 0;// 추상 메소드 2
-	void concreteOperation()// 구상 메소드
-	{
-		std::cout << "구상 함수입니다." << std::endl;
+	virtual void brew() = 0;
+	virtual void addCondiments() = 0;
+	void boilWater() {
+		std::cout << "Boiling water" << std::endl;
+	}
+	void pourInCup() {
+		std::cout << "Pouring into cup" << std::endl;
+	}
+	virtual bool customerWantsCondiments() { // hook
+		return true;
 	}
 };
 
