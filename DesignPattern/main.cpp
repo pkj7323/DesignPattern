e1668147124aded93f9016c7d9538a5f679fcb6d
pkj7323 <pkj7323@naver.com>
2025-01-25 15:39:34 +0900

    티를 끓여먹으려면?

--- DesignPattern/main.cpp
+++ DesignPattern/main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
+class Tea
+{
+public:
+	void prepareRecipe()
+	{
+		boilWater();
+		steepTeaBag();
+		pourInCup();
+		addLemon();
+	}
+	void boilWater()
+	{
+		std::cout << "Boiling water" << std::endl;
+	}
+	void steepTeaBag()
+	{
+		std::cout << "Steeping the tea" << std::endl;
+	}
+	void pourInCup()
+	{
+		std::cout << "Pouring into cup" << std::endl;
+	}
+	void addLemon()
+	{
+		std::cout << "Adding Lemon" << std::endl;
+	}
+};
+
+
+
+
+
 int main() {
 	
 	return 0;
