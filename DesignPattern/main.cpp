59b57f5f4992197b4ff55069739a5a04e118eb76
pkj7323 <pkj7323@naver.com>
2025-02-21 15:35:24 +0900

    오리 하나 만들기

---
+++ DesignPattern/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Quackable
+{
+public:
+	virtual void quack() = 0;
+};
+
+class MallardDuck : public Quackable
+{
+public:
+	void quack() override
+	{
+		std::cout << "²Ð²Ð" << std::endl;
+	}
+};
